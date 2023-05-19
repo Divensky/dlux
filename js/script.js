a390361409570d2786240546649a07bef55f956c
Irina Kats <irina8kats@gmail.com>
2023-05-19 11:16:44 -0400

    fix: conditional statements added in js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-let toggle = document.querySelector(".toggle");
-let menu = document.querySelector("header");
+let toggle = document.querySelector("#toggle");
+let menu = document.querySelector("#main-header");
+
+if (toggle && menu) {
+    /* Event Listener */
+    toggle.addEventListener("click", () => {
+        toggleMenu(toggle, menu);
+    }, false);
+}
+
 
 /* Toggle flyout menu */
-function toggleMenu() {
+function toggleMenu(toggle, menu) {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
-            // change the menu icon to mobile menu
-            toggle.querySelector("img").src = "img/icon_menu.svg";
+
+        // change the menu icon to mobile menu
+        toggle.querySelector("img").src = "img/icon_menu.svg";        
     } else {
         menu.classList.add("active");
 
@@ -14,5 +23,3 @@ function toggleMenu() {
         toggle.querySelector("img").src = "img/icon_exit_menu.svg";
     }
 }
-/* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
